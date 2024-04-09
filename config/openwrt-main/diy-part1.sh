a2cd2db7a17105b9a733962cdffcdab798f37cdc
shui100 <39903018+shui100@users.noreply.github.com>
2024-04-09 16:52:32 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -10,7 +10,7 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '3i src-git passwall1 https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '3i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
